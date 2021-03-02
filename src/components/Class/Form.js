9cff28e6f3a40c92ab8052b7da6bbbb9e82f825f
cduggin <ephdisc@gmail.com>
2021-03-02 10:43:02 -0800

    -

--- src/components/Class/Form.js
+++ src/components/Class/Form.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
+/**
+ * Task:
+ * 1. Create a basic form with a labelled 'name' field.
+ * 2. Create a Label that is tied to the Input Field.
+ * 3. Create an Input field where the value is managed by the state.
+ * 4. When the Submit button is clicked, alert() with the Name's value.
+ *
+ *
+ * Name: [        ]
+ * [ Submit ]
+ *
+ */
+
+
 export default class Form extends Component {
     constructor(props) {
         super(props);
+
     }
 
     render() {
         return null
     }
 }
-
--- src/components/Func/Form.js
+++ src/components/Func/Form.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+/**
+ * Task:
+ * 1. Create a basic form with a labelled 'name' field.
+ * 2. Create a Label that is tied to the Input Field.
+ * 3. Create an Input field where the value is managed by the state.
+ * 4. When the Submit button is clicked, alert() with the Name's value.
+ *
+ *
+ * Name: [        ]
+ * [ Submit ]
+ *
+ */
+
 export default function(props) {
     return null;
 }
